5b458742733fc5c4ff9715d389f48b9ded5db392
JoseALermaIII <jose@JoseALerma.com>
2019-05-06 03:44:12 -0500

    Initial commit

---
+++ src/ch01/practice/p1_pig_latin.py
@@ -0,0 +1,6 @@
+"""Takes a word as input and returns its Pig Latin equivalent."""
+
+def main():
+    """If a word begins with a consonant, move the consonant to the end of the
+    word and add 'ay' to the end of the new word. If a word begins with a
+    vowel, add 'way' to the end of the word."""
