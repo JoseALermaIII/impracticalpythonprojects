f8d8198cf86b706d0becadfc0522565652db4771
JoseALermaIII <jose@JoseALerma.com>
2019-05-06 17:21:50 -0500

    Added outline

--- src/ch01/practice/p1_pig_latin.py
+++ src/ch01/practice/p1_pig_latin.py
@@ -4,3 +4,10 @@ def main():
     """If a word begins with a consonant, move the consonant to the end of the
     word and add 'ay' to the end of the new word. If a word begins with a
     vowel, add 'way' to the end of the word."""
+
+    # TODO: Check if word starts with a vowel.
+
+    # TODO: If so, append 'way' and return result.
+
+    # TODO: If not, move the consonant to the end of the word, add 'ay', and
+    # return the result
