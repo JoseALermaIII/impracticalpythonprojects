db3b639b2067ddb373f606ea3ed0d19317a554c1
JoseALermaIII <jose@JoseALerma.com>
2019-05-06 20:03:58 -0500

    Working solution

--- src/ch01/practice/p1_pig_latin.py
+++ src/ch01/practice/p1_pig_latin.py
@@ -1,13 +1,44 @@
 """Takes a word as input and returns its Pig Latin equivalent."""
 
-def main():
-    """If a word begins with a consonant, move the consonant to the end of the
+def encode(word: str) -> str:
+    """Check if word starts with vowel, then translate to Pig Latin.
+
+    If a word begins with a consonant, move the consonant to the end of the
     word and add 'ay' to the end of the new word. If a word begins with a
-    vowel, add 'way' to the end of the word."""
+    vowel, add 'way' to the end of the word.
 
-    # TODO: Check if word starts with a vowel.
+    Args:
+        word: Word to encode to Pig Latin.
 
-    # TODO: If so, append 'way' and return result.
+    Returns:
+        Encoded Pig Latin word.
 
-    # TODO: If not, move the consonant to the end of the word, add 'ay', and
+    """
+    vowels = ('a', 'e', 'i', 'o', 'u')
+
+    # Check if word starts with a vowel.
+    if word.lower().startswith(vowels):
+        # If so, append 'way' and return result.
+        return word + 'way'
+
+    # If not, move the consonant to the end of the word, add 'ay', and
     # return the result
+    word = word[1:] + word[0] + 'ay'
+    return word
+
+
+def main():
+    """Demonstrate Pig Latin encoder."""
+    print("This is a Pig Latin encoder.")
+
+    while True:
+        word = input("\nEnter a word to encode to Pig Latin: ")
+        print(f"\nTranslation: {encode(word)}")
+
+        retry = input("\nTry again? (Y/N) ")
+        if retry.lower().startswith('n'):
+            print("\nThank you, and good bye.")
+            break
+
+if __name__ == '__main__':
+    main()
