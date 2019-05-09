c34883cdd358eb3599d10c21f0da2afd296ba1d2
jose <jose@JoseALerma.com>
2019-05-09 03:42:55 -0500

    Initialize TestBarChart with test_bad_type

--- tests/test_chapter01.py
+++ tests/test_chapter01.py
@@ -1,7 +1,8 @@
 """Test Chapter 1."""
 import unittest
 import src.ch01.practice.p1_pig_latin as pig_latin
-from src.ch01.practice import ENCODE_ERROR
+import src.ch01.practice.p2_poor_bar_chart as bar_chart
+from src.ch01.practice import ENCODE_ERROR, FREQ_ANALYSIS_ERROR, PRINT_BAR_CHART_ERROR
 
 
 class TestPigLatin(unittest.TestCase):
@@ -24,6 +25,20 @@ class TestPigLatin(unittest.TestCase):
     def test_upper_to_lower(self):
         """Test that it converts uppercase to lowercase."""
         self.assertEqual(pig_latin.encode('Jose'), 'osejay')
+
+
+class TestBarChart(unittest.TestCase):
+    """Test Poor Bar Chart."""
+
+    def test_bad_type(self):
+        """Test that it raises an error if sentence is not a string nor
+        if freq_dict is not a dictionary."""
+        with self.assertRaises(TypeError) as err:
+            bar_chart.freq_analysis(3)
+            self.assertEqual(FREQ_ANALYSIS_ERROR, err.exception)
+        with self.assertRaises(TypeError) as err:
+            bar_chart.print_bar_chart(4)
+            self.assertEqual(PRINT_BAR_CHART_ERROR, err.exception)
 
 
 if __name__ == '__main__':
