ead2ad988a52d8ff443157e5689d523b19fcc34b
jose <jose@JoseALerma.com>
2019-05-09 03:42:02 -0500

    Working solution

--- src/ch01/practice/__init__.py
+++ src/ch01/practice/__init__.py
@@ -6,8 +6,16 @@ Attributes:
 
     ENCODE_ERROR (str): String with TypeError for Pig Latin Encoder
 
+    FREQ_ANALYSIS_ERROR (str): String with TypeError for Poor Bar Chart
+        frequency analysis
+
+    PRINT_BAR_CHART_ERROR (str): String with TypeError for Poor Bar Chart
+        printer.
+
 """
 
 # Constants
 VOWELS = ('a', 'e', 'i', 'o', 'u')
 ENCODE_ERROR = 'Word must be a string.'
+FREQ_ANALYSIS_ERROR = 'Sentence must be a string.'
+PRINT_BAR_CHART_ERROR = 'Object must be a dictionary.'
--- src/ch01/practice/p2_poor_bar_chart.py
+++ src/ch01/practice/p2_poor_bar_chart.py
@@ -1,6 +1,7 @@
 """Takes a sentence as input and returns a 'bar chart' of each letter."""
-import collections
 import pprint
+from collections import defaultdict
+from src.ch01.practice import FREQ_ANALYSIS_ERROR, PRINT_BAR_CHART_ERROR
 
 
 def freq_analysis(sentence: str) -> dict:
@@ -16,8 +17,26 @@ def freq_analysis(sentence: str) -> dict:
         Dictionary with each letter as keys and list with letters repeated
         based on their frequency as values.
 
+    Raises:
+        TypeError: If `sentence` is not a string.
+
     """
-    #  TODO: Add each letter to a list with collections.defaultdict
+    if not isinstance(sentence, str):
+        raise TypeError(FREQ_ANALYSIS_ERROR)
+
+    #  Add each letter to a list with collections.defaultdict
+    output = defaultdict(list)
+
+    for i in sentence:
+        if not i.isalpha():
+            continue
+
+        if i.isupper():
+            i = i.lower()
+
+        output[i].append(i)
+
+    return output
 
 
 def print_bar_chart(freq_dict: dict) -> None:
@@ -31,14 +50,26 @@ def print_bar_chart(freq_dict: dict) -> None:
         freq_analysis.
 
     Returns:
-        None. Prints dictionary to terminal.
+        If recursive, prints a recursive-safe string, otherwise prints the
+        dictionary.
+
+    Raises:
+        TypeError: If `freq_dict` is not a dictionary.
 
     """
-    #  TODO: Display dictionary with pprint
+    if not isinstance(freq_dict, dict):
+        raise TypeError(PRINT_BAR_CHART_ERROR)
+
+    if pprint.isrecursive(freq_dict):
+        return pprint.pprint(pprint.saferepr(freq_dict))
+
+    return pprint.pprint(freq_dict)
 
 
 def main():
     """Demonstrates the Poor Bar Chart."""
+    twister = 'Sally sells seashells by the seashore.'
+    print_bar_chart(freq_analysis(twister))
 
 
 if __name__ == '__main__':
