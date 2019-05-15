0059e32797c7cb1726bd4628fbc93437c5dbc2f6
JoseALermaIII <jose@JoseALerma.com>
2019-05-14 20:15:35 -0500

    Working solution

---
+++ src/ch01/challenge/__init__.py
@@ -0,0 +1,9 @@
+"""Chapter 1 Challenge Projects.
+
+Attributes:
+    ADD_KEYS_ERROR (str): String with TypeError for Foreign Bar Chart.
+
+"""
+
+# Constants
+ADD_KEYS_ERROR = 'Input must be a dictionary.'
---
+++ src/ch01/challenge/c1_foreign_bar_chart.py
@@ -0,0 +1,64 @@
+"""Takes a non-English sentence as input and returns a 'bar chart' of each
+letter."""
+import string
+from src.ch01.challenge import ADD_KEYS_ERROR
+from src.ch01.practice.p2_poor_bar_chart import freq_analysis, print_bar_chart
+
+
+def add_keys_to_dict(dictionary: dict) -> dict:
+    """Add keys to dictionary.
+
+    Check keys of a letter dictionary and add missing letters.
+
+    Args:
+        dictionary (dict): Dictionary to check keys of.
+
+    Returns:
+        Dictionary with all ASCII lowercase letters as keys.
+
+    Raises:
+        TypeError: If `dictionary` is not a dictionary.
+
+    """
+    if not isinstance(dictionary, dict):
+        raise TypeError(ADD_KEYS_ERROR)
+
+    for i in string.ascii_lowercase:
+        if i not in dictionary:
+            dictionary[i] = []
+
+    return dictionary
+
+
+def foreign_freq_analysis(sentence: str) -> dict:
+    """Wrapper for freq_analysis and add_keys_to_dict.
+
+    Passes given sentence through freq_analysis then add_keys_to_dict to
+    fill in missing keys.
+
+    Args:
+        sentence (str): String to count letters of.
+
+    Returns:
+        Dictionary with all ASCII lowercase letters as keys and list with
+        letters repeated based on their frequency as values.
+
+    Raises:
+        TypeError: If `sentence` is not a string.
+
+    """
+    return add_keys_to_dict(freq_analysis(sentence))
+
+
+def main():
+    """Demonstrates the Foreign Bar Chart."""
+    print('Este es un gráfico de barras asequible para idiomas extranjeros.\n')
+    trabalengua = 'Sally vende conchas al lado del orilla del mar.'
+    print(f'Analizando: {trabalengua}\n')
+    print_bar_chart(foreign_freq_analysis(trabalengua))
+    print('\nSi inclina su cabeza hacia la derecha, la letra "l" es la más '
+          'frecuente.')
+
+
+if __name__ == '__main__':
+    main()
