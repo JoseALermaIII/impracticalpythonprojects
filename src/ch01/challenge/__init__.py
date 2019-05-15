7cd4856cdb2dac9c72b2010adf49689a165ffe77
JoseALermaIII <jose@JoseALerma.com>
2019-05-15 02:12:30 -0500

    Add SPLIT_FROM_FILE_ERROR

--- src/ch01/challenge/__init__.py
+++ src/ch01/challenge/__init__.py
@@ -2,8 +2,10 @@
 
 Attributes:
     ADD_KEYS_ERROR (str): String with TypeError for Foreign Bar Chart.
+    SPLIT_FROM_FILE_ERROR (str): String with EOFError for Name Generator.
 
 """
 
 # Constants
 ADD_KEYS_ERROR = 'Input must be a dictionary.'
+SPLIT_FROM_FILE_ERROR = 'File must not be empty.'
