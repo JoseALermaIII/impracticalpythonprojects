aa85f18f9ebb66dd483edc6ea88308d3fe1874e1
JoseALermaIII <jose@JoseALerma.com>
2019-05-17 04:50:23 -0500

    Add BUILD_LIST_ERROR

--- src/ch01/challenge/__init__.py
+++ src/ch01/challenge/__init__.py
@@ -7,6 +7,7 @@ Attributes:
     SPLIT_NAME_EMPTY_ERROR (str): Sting with ValueError for Name Generator.
     ADD_NAME_TO_KEY_ERROR (str): String with TypeError for Name Generator.
     GENERATE_NAME_ERROR (str): String with KeyError for Name Generator.
+    BUILD_LIST_ERROR (str): String with IndexError for Name Generator.
 
 """
 
@@ -18,3 +19,4 @@ SPLIT_NAME_EMPTY_ERROR = 'List must not be empty.'
 ADD_NAME_TO_KEY_ERROR = 'Name and key must be a string and dictionary must ' \
                         'be a dictionary.'
 GENERATE_NAME_ERROR = 'Dictionary needs three keys.'
+BUILD_LIST_ERROR = 'Folder must not be empty.'
