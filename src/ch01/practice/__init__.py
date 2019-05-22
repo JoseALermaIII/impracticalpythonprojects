3e92ebdd14c836caf2c2465897085d0d513236da
JoseALermaIII <jose@JoseALerma.com>
2019-05-22 15:28:59 -0500

    Add sphinx references to docstring

--- src/ch01/practice/__init__.py
+++ src/ch01/practice/__init__.py
@@ -4,13 +4,14 @@ Attributes:
     VOWELS (tuple): Tuple containing characters of the English vowels
         (except for 'y')
 
-    ENCODE_ERROR (str): String with TypeError for Pig Latin Encoder
+    ENCODE_ERROR (str): String with :py:exc:`TypeError` for Pig Latin
+        :func:`~p1_pig_latin.encode`.
 
-    FREQ_ANALYSIS_ERROR (str): String with TypeError for Poor Bar Chart
-        frequency analysis
+    FREQ_ANALYSIS_ERROR (str): String with :py:exc:`TypeError` for Poor Bar
+        Chart :func:`~p2_poor_bar_chart.freq_analysis`.
 
-    PRINT_BAR_CHART_ERROR (str): String with TypeError for Poor Bar Chart
-        printer.
+    PRINT_BAR_CHART_ERROR (str): String with :py:exc:`TypeError` for Poor
+        Bar Chart :func:`~p2_poor_bar_chart.print_bar_chart`.
 
 """
 
