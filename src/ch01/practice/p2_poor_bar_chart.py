13194e655ca33d7205d8189c28bdac24a9c0e257
JoseALermaIII <jose@JoseALerma.com>
2019-05-22 15:41:18 -0500

    Add sphinx references to docstrings

--- src/ch01/practice/p2_poor_bar_chart.py
+++ src/ch01/practice/p2_poor_bar_chart.py
@@ -8,14 +8,14 @@ def freq_analysis(sentence: str) -> dict:
     """Perform frequency analysis of letters in sentence.
 
     Iterate through each letter in the sentence and add it to a dictionary of
-    lists using ``collections.defaultdict``.
+    lists using :py:class:`collections.defaultdict`.
 
     Args:
         sentence (str): String to count letters of.
 
     Returns:
-        Dictionary with each letter as keys and list with letters repeated
-        based on their frequency as values.
+        :py:class:`~collections.defaultdict` with each letter as keys and list
+        with letters repeated based on their frequency as values.
 
     Example:
         >>> from src.ch01.practice.p2_poor_bar_chart import freq_analysis
@@ -50,16 +50,16 @@ def freq_analysis(sentence: str) -> dict:
 def print_bar_chart(freq_dict: dict) -> None:
     """Print dictionary to terminal.
 
-    Use ``pprint`` to print dictionary with letter frequency analysis to
-    terminal.
+    Use :py:func:`pprint.pprint` to print dictionary with letter frequency
+    analysis to terminal.
 
     Args:
         freq_dict (dict): Dictionary with frequency analysis from
-        freq_analysis.
+            :func:`freq_analysis`.
 
     Returns:
-        If recursive, prints a recursive-safe string, otherwise prints the
-        dictionary.
+        :py:obj:`None`. If recursive, prints a recursive-safe string,
+        otherwise prints the dictionary.
 
     Raises:
         TypeError: If `freq_dict` is not a dictionary.
