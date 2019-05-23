b87f9abfdd50255855f77e3de2899be72ae0a7f0
JoseALermaIII <jose@JoseALerma.com>
2019-05-23 00:49:02 -0500

    Refactor cleanup_list from cleanup_dict

--- src/ch02/p1_cleanup_dictionary.py
+++ src/ch02/p1_cleanup_dictionary.py
@@ -3,20 +3,37 @@ from src.ch01.challenge.c2_name_generator import read_from_file
 from src.ch02 import DICTIONARY_FILE_PATH
 
 
-def cleanup_dict(filepath: str) -> list:
-    """Cleanup dictionary.
+def cleanup_list(word_list: list) -> list:
+    """Cleanup word list.
 
-    Remove single letter words from a word dictionary file.
+    Remove single letter words from a :py:obj:`list` of words.
 
     Args:
-        filepath (str): String with path to dictionary file.
+        word_list (list): List with words as elements.
 
     Returns:
         List with words as elements excluding single letter words.
 
     """
-    word_list = read_from_file(filepath)
     return [word for word in word_list if len(word) > 1]
+
+
+def cleanup_dict(filepath: str) -> list:
+    """Wrap read_from_file and cleanup_list.
+
+    Passes given **filepath** through
+    :func:`~src.ch01.challenge.c2_name_generator.read_from_file`
+    to get a list of words, then :func:`cleanup_list` to remove single letter
+    words.
+
+    Args:
+        filepath (str): String with path to word dictionary file.
+
+    Returns:
+        List with words as elements excluding single letter words.
+
+    """
+    return cleanup_list(read_from_file(filepath))
 
 
 def main():
