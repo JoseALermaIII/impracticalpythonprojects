b69285df02bb600850a84399e852a9c8a291e651
JoseALermaIII <jose@JoseALerma.com>
2019-05-24 03:44:36 -0500

    Add test_cleanup_list to TestCleanupDictionary

--- tests/test_chapter02.py
+++ tests/test_chapter02.py
@@ -1,6 +1,7 @@
 """Test Chapter 2."""
 import unittest
 import src.ch02.p1_cleanup_dictionary as cleanup_dictionary
+from tests import random_string
 from src.ch02 import CLEANUP_LIST_ERROR
 
 
@@ -12,6 +13,15 @@ class TestCleanupDictionary(unittest.TestCase):
         with self.assertRaises(IndexError) as err:
             cleanup_dictionary.cleanup_list([])
             self.assertEqual(CLEANUP_LIST_ERROR, err.exception)
+
+    def test_cleanup_list(self):
+        """Test that it removes single letter words from a list of words."""
+        random_list = [random_string(1) for _ in range(13)]
+        random_list.extend([random_string(5) for _ in range(10)])
+        clean_list = cleanup_dictionary.cleanup_list(random_list)
+        self.assertEqual(len(clean_list), 10)
+        for element in clean_list:
+            self.assertEqual(len(element), 5)
 
 
 if __name__ == '__main__':
