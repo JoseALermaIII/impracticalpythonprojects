3dbe5fd199b09769981f5f2ad6ec92dc16ed913c
JoseALermaIII <jose@JoseALerma.com>
2019-05-25 03:59:16 -0500

    Add RECURSIVE_ISPALINDROME_ERROR

--- src/ch02/__init__.py
+++ src/ch02/__init__.py
@@ -7,8 +7,13 @@ Attributes:
     CLEANUP_LIST_ERROR (str): String with :py:exc:`IndexError` for Cleanup
         Dictionary :func:`~p1_cleanup_dictionary.cleanup_list`.
 
+    RECURSIVE_ISPALINDROME_ERROR (str): String with :py:exc:`TypeError` for
+        Recursive Palindrome
+        :func:`~c1_recursive_palindrome.recursive_ispalindrome`.
+
 """
 
 # Constants
 DICTIONARY_FILE_PATH = '/usr/share/dict/american-english'
 CLEANUP_LIST_ERROR = 'List cannot be empty.'
+RECURSIVE_ISPALINDROME_ERROR = 'Word must be a string.'
