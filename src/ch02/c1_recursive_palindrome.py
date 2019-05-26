bc1ef5303af82bd2a56faf2ba782a092fb426987
JoseALermaIII <jose@JoseALerma.com>
2019-05-26 03:08:41 -0500

    Working solution

--- src/ch02/c1_recursive_palindrome.py
+++ src/ch02/c1_recursive_palindrome.py
@@ -19,17 +19,29 @@ def recursive_ispalindrome(word: str) -> bool:
         raise TypeError(RECURSIVE_ISPALINDROME_ERROR)
 
     # Base case: if no letters or one letter, return True.
+    if len(word) <= 1:
+        return True
 
     # Recursive loop: if first and last letters are different, return False.
     # Otherwise, remove the first and last letters and call function again.
+    if word[0] == word[-1]:
+        return recursive_ispalindrome(word[1:-1])
+    return False
 
 
 def main():
     """Demonstrate the recursive palindrome tester."""
     print('I\'m a recursive palindrome tester.\n'
-          'I basically call on myself repeatedly to check if a string is a'
+          'I basically call on myself repeatedly to check if a string is a '
           'palindrome.\n'
           'Incidentally, my favorite food is Random Access Memory.\n')
+    palindrome = 'rotor'
+    print(f'Analyzing: {palindrome}\n')
+
+    if recursive_ispalindrome(palindrome):
+        print(f'I do declare that "{palindrome}" is a palindrome!')
+    else:
+        print(f'I\'m afraid that "{palindrome}" isn\'t a palindrome 😞')
 
 
 if __name__ == '__main__':
