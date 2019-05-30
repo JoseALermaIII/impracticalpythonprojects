d89a5968873300f98825d21008790297d31d4820
JoseALermaIII <jose@JoseALerma.com>
2019-05-30 01:03:31 -0500

    Add test_digram_counter to TestDigramCounter

--- tests/test_chapter03.py
+++ tests/test_chapter03.py
@@ -1,4 +1,5 @@
 """Test Chapter 3."""
+import os
 import unittest
 import src.ch03.p1_digram_counter as digram_counter
 from src.ch03 import GET_DIGRAMS_ERROR, COUNT_DIGRAMS_ERROR
@@ -31,6 +32,17 @@ class TestDigramCounter(unittest.TestCase):
         digram_count = {'mo': 2, 'gh': 1, 'li': 1, 'to': 1, 'on': 1, 'bo': 0}
         test_count = digram_counter.count_digrams(digrams, word_list)
         self.assertDictEqual(digram_count, test_count)
+
+    def test_digram_counter(self):
+        """Test that it can count digrams in a word dictionary file."""
+        test_dict_path = os.path.abspath('tests/data/ch02/dictionary.txt')
+        word = 'abracadabra'
+        count = {'ra': 4, 'ar': 4, 'ac': 3, 'ca': 3, 'br': 2, 'ab': 1,
+                 'da': 1, 'ad': 1, 'rd': 1, 'dr': 1, 'bb': 1, 'rr': 1,
+                 'aa': 1, 'bc': 0, 'dc': 0, 'cd': 0, 'ba': 0, 'db': 0,
+                 'bd': 0, 'cr': 0, 'rb': 0, 'cb': 0, 'rc': 0}
+        test_count = digram_counter.digram_counter(word, test_dict_path)
+        self.assertDictEqual(count, test_count)
 
 
 if __name__ == '__main__':
