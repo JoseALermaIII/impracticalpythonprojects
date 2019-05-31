788e8d0e503e0568a486ee0cbe962f92bd993294
JoseALermaIII <jose@JoseALerma.com>
2019-05-31 05:01:16 -0500

    Initial commit

---
+++ src/ch03/c1_anagram_generator.py
@@ -0,0 +1,30 @@
+"""Generate phrase anagrams from a word or phrase."""
+
+
+def anagram_generator(word: str) -> list:
+    """Generate phrase anagrams.
+
+    Make phrase anagrams from a given word or phrase.
+
+    Args:
+        word (str): Word to get phrase anagrams of.
+
+    Returns:
+        :py:obj:`list` of phrase anagrams of **word**.
+
+    """
+    # Given a word, build a list of anagrams.
+    # For each anagram in the list, build a phrase anagram by getting the
+    # anagram's anagrams.
+    pass
+
+
+def main():
+    """Demonstrate the Anagram Generator."""
+    print('I can find all phrase anagrams given a word or phrase.\n'
+          'I\'m fun at parties.')
+    # Print first 500 results.
+
+
+if __name__ == '__main__':
+    main()
