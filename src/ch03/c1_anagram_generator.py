31a8164b9bebd5e27fc82f2b949b3ce91a37a614
JoseALermaIII <jose@JoseALerma.com>
2019-05-31 05:02:48 -0500

    Add get_primes

--- src/ch03/c1_anagram_generator.py
+++ src/ch03/c1_anagram_generator.py
@@ -1,6 +1,31 @@
 """Generate phrase anagrams from a word or phrase."""
 
 
+def get_primes(n: int = 26, max_prime: int = 101) -> list:
+    """Get list of primes.
+
+    Given a number and maximum prime number, return a list of prime numbers.
+
+    Args:
+        n (int): Number of prime numbers to return. Defaults to ``26``.
+        max_prime (int): Largest prime number to return. Defaults to ``101``.
+
+    Returns:
+        :py:obj:`list` of **n** prime numbers with **max_prime** as the
+        largest prime number in the list.
+
+    """
+    primes = [2]
+    while len(primes) <= n:
+        # Skip 2 and iterate over odd numbers.
+        for num in range(3, max_prime + 1, 2):
+            #  If num can't be divided by all odd numbers from 3 to sqrt(num),
+            #  it's a prime.
+            if all(num % i != 0 for i in range(3, int(num**.5) + 1, 2)):
+                primes.append(num)
+    return primes
+
+
 def anagram_generator(word: str) -> list:
     """Generate phrase anagrams.
 
