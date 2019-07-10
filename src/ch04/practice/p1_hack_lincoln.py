e212f5f8adffdb97ebc4b52e4d6b2bba3d35eef7
JoseALermaIII <jose@JoseALerma.com>
2019-07-10 00:52:59 -0500

    Initial commit

---
+++ src/ch04/practice/p1_hack_lincoln.py
@@ -0,0 +1,8 @@
+"""Hack route cipher sent by Abraham Lincoln."""
+
+
+def main():
+    """Demonstrate hack of Lincoln's route cipher."""
+    ciphertext = """THIS OFF DETAINED ASCERTAIN WAYLAND CORRESPONDENTS OF AT
+    WHY AND IF FILLS IT YOU GET THEY NEPTUNE THE TRIBUNE PLEASE ARE THEM CAN 
+    UP"""
