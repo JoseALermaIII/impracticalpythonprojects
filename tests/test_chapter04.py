49ca8e6b24c2de9d0d29f8ee3ce447298b3610e2
JoseALermaIII <jose@JoseALerma.com>
2019-07-16 05:44:15 -0500

    Add test_keygen to TestHackLincoln

--- tests/test_chapter04.py
+++ tests/test_chapter04.py
@@ -21,6 +21,26 @@ class TestHackLincoln(unittest.TestCase):
         test_factors = hack_lincoln.get_factors(32)
         self.assertListEqual(factors, test_factors)
 
+    def test_keygen(self):
+        """Test keygen."""
+        # Test small key length
+        keys = [[-1, 2], [1, -2], [1, 2], [-1, -2]]
+        test_keys = hack_lincoln.keygen(2)
+        self.assertListEqual(keys, test_keys)
+        # Test odd key length
+        keys = [[1, 2, -3], [-1, 2, 3], [-1, -2, 3], [1, -2, -3],
+                [-1, -2, -3], [-1, 2, -3], [1, -2, 3], [1, 2, 3]]
+        test_keys = hack_lincoln.keygen(3)
+        self.assertListEqual(keys, test_keys)
+        # Test big key length
+        keys = [[-1, -2, 3, 4], [-1, 2, -3, 4], [-1, -2, 3, -4],
+                [-1, 2, 3, -4], [-1, -2, -3, -4], [1, -2, -3, 4],
+                [1, -2, 3, 4], [1, 2, -3, 4], [1, 2, 3, 4], [1, -2, 3, -4],
+                [1, -2, -3, -4], [1, 2, -3, -4], [-1, -2, -3, 4],
+                [1, 2, 3, -4], [-1, 2, 3, 4], [-1, 2, -3, -4]]
+        test_keys = hack_lincoln.keygen(4)
+        self.assertListEqual(keys, test_keys)
+
 
 if __name__ == '__main__':
     unittest.main()
