26cc46082aae338139d9c89e0f33c60aab5e393d
JoseALermaIII <jose@JoseALerma.com>
2019-07-16 05:43:44 -0500

    Add keygen

--- src/ch04/practice/p1_hack_lincoln.py
+++ src/ch04/practice/p1_hack_lincoln.py
@@ -1,4 +1,5 @@
 """Hack route cipher sent by Abraham Lincoln."""
+from itertools import permutations
 
 
 def get_factors(integer: int) -> list:
@@ -21,6 +22,33 @@ def get_factors(integer: int) -> list:
             # If you have one factor, the other is integer / factor
             result.append(integer // i)
     return list(set(result))  # Eliminate perfect squares
+
+
+def keygen(length: int) -> list:
+    """Generate all possible route cipher keys.
+
+    Generates a list of all possible route cipher keys of **length**.
+
+    Args:
+        length (int): Length of route cipher key.
+
+    Returns:
+        List of lists of integers representing all possible route cipher keys
+        of **length**.
+
+    Example:
+        >>> from src.ch04.practice.p1_hack_lincoln import keygen
+        >>> keygen(2)
+        [[-1, 2], [1, -2], [1, 2], [-1, -2]]
+
+    """
+    result = []
+    master_key = range(1, length + 1)
+    # Get all possible combinations of direction (pos/neg) of length
+    perms = set(permutations([-1, 1] * length, length))  # Remove repeats
+    for perm in perms:
+        result.append([sign * key for sign, key in zip(perm, master_key)])
+    return result
 
 
 def main():
