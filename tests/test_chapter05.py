4dc1715ee19d2631eb1bc331488f711a7d52176a
JoseALermaIII <jose@JoseALerma.com>
2019-08-20 19:23:01 -0500

    Add test_main to TestEncodeNull

--- tests/test_chapter05.py
+++ tests/test_chapter05.py
@@ -1,4 +1,5 @@
 """Test Chapter 5."""
+import os
 import unittest.mock
 from io import StringIO
 import src.ch05.p1_encode_null as encode_null
@@ -27,6 +28,26 @@ class TestEncodeNull(unittest.TestCase):
         answerlist = ['Loki', 'Stout', 'Lee', 'Tsar', 'Scrooge',
                       'Ruth', 'Smoke', 'Nero', 'Vest', 'Egg']
         self.assertListEqual(testlist, answerlist)
+
+    @unittest.mock.patch('src.ch05.p1_encode_null.split_names')
+    @unittest.mock.patch('src.ch05.p1_encode_null.os.path.abspath')
+    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+    def test_main(self, mock_stdout, mock_abspath, mock_split_names):
+        """Test demo main function."""
+        # Mock output of abspath to avoid FileNotFoundError.
+        mock_abspath.return_value = os.path.normpath('src/ch01/challenge/c2files')
+        # Mock output of split_names for test consistency.
+        mock_split_names.return_value = \
+            {'last': ['Abumrad', 'Abando', 'Dyke', 'Anthony', 'Chapman',
+                      'Breeck', 'Brooks', 'Armstrong', 'Anderson', 'Baker',
+                      'Bennington', 'Edwards', 'Dewees', 'Belushi', 'April',
+                      'Cicierega', 'Brookshier']}
+        encode_null.main()
+        # Test printed output.
+        with open(os.path.normpath('tests/data/ch05/main/encode_null.txt'),
+                  'r') as file:
+            file_data = ''.join(file.readlines())
+        self.assertEqual(mock_stdout.getvalue(), file_data)
 
 
 if __name__ == '__main__':
