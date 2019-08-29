b08235e31cf8db10909d35f5a0676c3fd3af7441
JoseALermaIII <jose@JoseALerma.com>
2019-08-29 00:28:13 -0500

    Add another test to test_get_text

--- tests/test_chapter06.py
+++ tests/test_chapter06.py
@@ -17,6 +17,17 @@ class TestInvisibleInk(unittest.TestCase):
         # Test that it does skip blanks.
         paragraphs = invisible_ink.get_text(testfile)
         self.assertEqual(paragraphs.count(''), 0)
+        # Test that it read contents.
+        answerlist = \
+            ['This is a test document.',
+             'This is a paragraph with two runs. However, it’s not because it has two '
+             'lines.',
+             'There is intentionally a lot of blank spaces to check if the code can count '
+             'them correctly.',
+             'So, don’t send me e-mails saying that the formatting in my test files is '
+             'incorrect.',
+             'Word.']
+        self.assertEqual(answerlist, paragraphs)
 
 
 if __name__ == '__main__':
