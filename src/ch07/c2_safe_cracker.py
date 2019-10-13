7eac1bdcba2bde2b47a3c36fa819e7edf45a4c97
JoseALermaIII <jose@JoseALerma.com>
2019-10-13 12:13:26 -0500

    Add compare

--- src/ch07/c2_safe_cracker.py
+++ src/ch07/c2_safe_cracker.py
@@ -9,3 +9,20 @@ respond so, the change would be discarded. This improves upon the algorithm by
 removing the locked tumbler from the pool of tumblers to randomly change.
 
 """
+
+
+def compare(combo: list, attempt: list) -> int:
+    """Compare items in two lists and count number of matches.
+
+    Compare each element in **combo** with **attempt** and return
+    the number of matches.
+
+    Args:
+        combo (list): Integers of safe combination.
+        attempt (list): Integers of guessed safe combination.
+
+    Returns:
+        Number of combination matches between **combo** and **attempt**.
+
+    """
+    return sum(1 for i, j in zip(combo, attempt) if i == j)
