c2127bdc5e1d6eeb8e9b56081d6dae30364925b3
JoseALermaIII <jose@JoseALerma.com>
2019-10-13 14:09:51 -0500

    Fix punctuation in module docstring

--- src/ch07/c2_safe_cracker.py
+++ src/ch07/c2_safe_cracker.py
@@ -5,7 +5,7 @@ by one and noting whether the safe had a response. If so, lock the
 tumbler at that value and continue randomly changing tumbler values.
 
 Previously, a locked tumbler can still be changed, but the safe wouldn't
-respond so, the change would be discarded. This improves upon the algorithm by
+respond, so the change would be discarded. This improves upon the algorithm by
 removing the locked tumbler from the pool of tumblers to randomly change.
 
 """
