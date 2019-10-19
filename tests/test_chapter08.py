941fe04536401ff05c4654cecc979214e53c2726
JoseALermaIII <jose@JoseALerma.com>
2019-10-18 19:59:40 -0500

    Add test_main and setUpClass to TestCountSyllables

--- tests/test_chapter08.py
+++ tests/test_chapter08.py
@@ -1,11 +1,19 @@
 """Test Chapter 8."""
 import unittest.mock
+import os
+from random import Random
+from io import StringIO
 
 import src.ch08.p1_count_syllables as count_syllables
 
 
 class TestCountSyllables(unittest.TestCase):
     """Test Count Syllables."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Configure attributes for use in this class only."""
+        cls.random = Random()
 
     def test_format_words(self):
         """Test format_words."""
@@ -26,6 +34,22 @@ class TestCountSyllables(unittest.TestCase):
         self.assertEqual(count_syllables.count_syllables(['tuxes']), 2)
         # Test word in CMUdict.
         self.assertEqual(count_syllables.count_syllables(['test']), 1)
+
+    @unittest.mock.patch('src.ch08.p1_count_syllables.DICTIONARY_FILE_PATH', 'tests/data/ch08/dictionary.txt')
+    @unittest.mock.patch('sys.stdout', new_callable=StringIO)
+    @unittest.mock.patch('src.ch08.p1_count_syllables.choice')
+    def test_main(self, mock_choice, mock_stdout):
+        """Test demo main function."""
+        self.random.seed(222)
+        mock_choice._mock_side_effect = self.random.choice
+
+        count_syllables.main()
+
+        # Test sys.stdout output.
+        with open(os.path.normpath('tests/data/ch08/main/count_syllables.txt'),
+                  'r') as file:
+            file_data = ''.join(file.readlines())
+        self.assertEqual(mock_stdout.getvalue(), file_data)
 
 
 if __name__ == '__main__':
