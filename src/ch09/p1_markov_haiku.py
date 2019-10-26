6b50d7263f16ed50ca1e60e70c85b7896757d2b0
JoseALermaIII <jose@JoseALerma.com>
2019-10-25 20:49:38 -0500

    Initial commit

---
+++ src/ch09/p1_markov_haiku.py
@@ -0,0 +1,49 @@
+"""Generate haiku with Markov chain analysis.
+
+Generate Markov models of 1st and 2nd order from a training text of haiku
+poems using a random word. Using these Markov models, select words that build
+three lines following a haiku syllable structure: 5-7-5. As each word is
+selected, the Markov chain is expanded to use the two preceding words to
+generate new models.
+
+If the word or phrase doesn't have a Markov model word that fits the syllable
+count, generate a new Markov model using another random word and select a word
+that does fit the syllable count.
+
+"""
+import sys
+import logging
+import random
+from collections import defaultdict
+from src.ch01.challenge.c2_name_generator import read_from_file
+from src.ch08.p1_count_syllables import format_words, count_syllables
+
+# Configure parent logger for ch09 module.
+#
+# NOTE: All modules that import this module and reference the same logger
+# object, 'markov_haiku', will use this parent logger.
+#
+# logging.disable()  # Disable logging. Comment out to enable.
+log = logging.getLogger('markov_haiku')
+log.setLevel(logging.DEBUG)
+# Make file handler that logs everything.
+fh = logging.FileHandler('markov_haiku.log')
+fh.setLevel(logging.DEBUG)
+# Make console handler that outputs INFO or above levels.
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# Make formatter and add it to the handlers
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+# Add the handlers to log.
+log.addHandler(ch)
+log.addHandler(fh)
+
+
+def main():
+    """Demonstrate Markov haiku maker."""
+
+
+if __name__ == '__main__':
+    main()
